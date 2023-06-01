3193818a1efadea633f9bdd63f6d40833a183a81
hdb1412 <jayli2558@gmail.com>
2023-06-02 01:50:34 +0900

    make sample to get input and make decision

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("hi");
+const age = parseInt(prompt("How old are you?"));
+
+if (isNaN(age) || age < 0) {
+    console.log("Please write positive number");
+} else if (age >= 18) {
+    console.log("You can drink");
+} else {
+    console.log("You are too young to drink");
+}
