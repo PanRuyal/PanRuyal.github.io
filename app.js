a15fc4affb7416d84a439d488d263713980a4e99
hyeondobin <dobinhyeon@gmail.com>
2024-01-03 11:17:23 +0900

    update page
    remove ask
    test a tag

--- app.js
+++
@@ -1,9 +0,0 @@
-const age = parseInt(prompt("How old are you?"));
-
-if (isNaN(age) || age < 0) {
-    console.log("Please write positive number");
-} else if (age >= 18) {
-    console.log("You can drink");
-} else {
-    console.log("You are too young to drink");
-}
